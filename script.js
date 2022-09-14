7f45e0d2207a6dae7dd0a5e55d37efcb7a42f9fb
md-rakibul-islam <raaakib0@gmail.com>
2022-09-15 02:17:13 +0600

    responsive for large device 3 col card

--- script.js
+++ script.js
@@ -134,7 +134,5 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = Math.floor((currentTime - startTime) / 1000);
-  // Math.floor(timeSpent);
-  // console.log(startTime);
-  document.getElementById("show-time").innerHTML = `${startTime  ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
